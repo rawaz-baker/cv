e77329eda240cca1e9d485fed9d3a7b1bb733aae
rawaz-baker <113205831+rawaz-baker@users.noreply.github.com>
2022-09-09 21:26:14 +0300

    Add files via upload

---
+++ dev.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <link rel="stylesheet" href="index.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebPage
+    </title>
+</head>
+<body>
+ <ul>  
+    <a href="">Course</a>
+<li><a href="">HOME</a></li>
+<li><a href="">ABOUT</a></li>
+<li><a href="">SERVICE</a></li>
+<li><a href="">DESIGN</a></li>
+<li><a href="">CONTACT</a></li>
+<input type="text"placeholder="Type to Text">
+<button>Resherch</button>
+</ul>
